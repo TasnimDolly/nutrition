34b759cf40e9cf2e59acf2c07787e486c1f8eb0d
Hannen Azzoz <hannenazzoz3@gmail.com>
2026-04-22 22:42:48 +0300

    fix demo

--- src/Dashboard/DeleteDialog.js
+++ src/Dashboard/DeleteDialog.js
@@ -1,35 +1,60 @@
+import { DialogTitle } from "@mui/material";
 
-import { DialogTitle } from '@mui/material';
-
-import {TextField } from '@mui/material';
-import { Dialog } from '@mui/material';
-import { DialogContent } from '@mui/material';
-import {DialogActions} from '@mui/material';
-import { Button,Box } from '@mui/material';
-import { Typography } from '@mui/material';
-export default function DeleteDialog({icon,open,onClose,onConfirm,title,content}) { 
-
+import { TextField } from "@mui/material";
+import { Dialog } from "@mui/material";
+import { DialogContent } from "@mui/material";
+import { DialogActions } from "@mui/material";
+import { Button, Box } from "@mui/material";
+import { Typography } from "@mui/material";
+export default function DeleteDialog({
+  icon,
+  open,
+  onClose,
+  onConfirm,
+  title,
+  content,
+}) {
   return (
-    <Dialog PaperProps={{sx:{background:"#FFFFFF",padding:'10px'}}} open={open} onClose={onClose} fullWidth maxWidth="sm" dir="rtl">
-      <DialogTitle sx={{margin:'5px auto' ,display:'flex',alignItems:'center',flexDirection:'column' ,justifyContent:'center'}}>
-<Box> {icon}</Box>
-     <Typography variant='h4' >   {title}</Typography> </DialogTitle>
+    <Dialog
+      PaperProps={{ sx: { background: "#FFFFFF", padding: "10px" } }}
+      open={open}
+      onClose={onClose}
+      fullWidth
+      maxWidth="sm"
+      dir="rtl"
+    >
+      <DialogTitle
+        sx={{
+          margin: "5px auto",
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <Box> {icon}</Box>
+        <Typography variant="h4"> {title}</Typography>{" "}
+      </DialogTitle>
       <DialogContent>
-                          <Typography variant='h2'sx={{ marginTop: 5 }}>  {content}
-</Typography>
-         
-
-        
+        <Typography variant="h2" sx={{ marginTop: 5 }}>
+          {" "}
+          {content}
+        </Typography>
       </DialogContent>
 
       <DialogActions>
-        <Button sx ={{background:'red'}} onClick={()=>{onConfirm()}} variant="contained">
-          تأكيد 
+        <Button
+          sx={{ background: "red" }}
+          onClick={() => {
+            onConfirm();
+          }}
+          variant="contained"
+        >
+          تأكيد
         </Button>
         <Button onClick={onClose} color="inherit">
           إلغاء
         </Button>
-        
       </DialogActions>
     </Dialog>
   );
